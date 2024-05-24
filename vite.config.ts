eb747e30b5f7d9cc1151247ec3a24cf2993d68f4
Pez Cuckow <email@pezcuckow.com>
2024-05-24 11:26:43 +0100

    Add sign to build output

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
             input: {
                 'index': resolve(__dirname, 'index.html'),
                 'tent-sheet': resolve(__dirname, 'tent-sheet/index.html'),
+                'temporary-sign': resolve(__dirname, 'temporary-sign/index.html'),
                 'markdown-panel': resolve(__dirname, 'markdown-panel/index.html'),
             },
         },
